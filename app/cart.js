d55c741f0d44f3ee2e74b9cb0a0334bf2dd96e04
jainam <jainambagrecha16@gmail.com>
2023-09-04 22:03:06 +0530

    product page and RelatedProducts component

---
+++ app/cart.js
@@ -0,0 +1,15 @@
+import Wrapper from '@/components/Wrapper';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react'
+
+
+const Cart = () => {
+  return (
+    <div>
+      cart
+    </div>
+  )
+}
+
+export default Cart;
